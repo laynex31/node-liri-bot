90a40f54ad4a7142388f44ee1f57fb49a796d490
Layne Watkins <laynex31@yahoo.com>
2019-05-11 14:54:23 -0500

    movie call finished, concert call is mapping and displaying most data

--- liri.js
+++ liri.js
@@ -3,56 +3,59 @@ require("dotenv").config();
  var Spotify = require('node-spotify-api');
  var spotify = new Spotify(keys.spotify);
  var axios = require("axios");
+ var term = process.argv.slice(3).join(" ");
+
+ function bandInfo(response) {
+  console.log("\nName of Venue: " +  response.venue.name + "\nLocation: " + response.venue.city + "," + response.venue.region) 
+
+
+  }
 
 
  if (process.argv[2] === "concert-this") {
 
-    var artist = process.argv[3];
+ 
 
-    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
+    axios.get("https://rest.bandsintown.com/artists/" + term + "/events?app_id=codingbootcamp").then(
   function(response) {
-    console.log(response);
+    
+   // console.log(response.data)
+    
+    response.data.map(bandInfo)
   }
 );
  };
 
-
  if (process.argv[2] === "spotify-this-song") {
 
-    spotify
-  .search({ type: 'track', query: process.argv[3] })
-  .then(function(response) {
-    console.log(response);
-  })
-  .catch(function(err) {
-    console.log(err);
-  })
- };
+  spotify
+.search({ type: 'track', query: term })
+.then(function(response) {
+  console.log(response);
+})
+.catch(function(err) {
+  console.log(err);
+})
+};
 
- if (process.argv[2] === "movie-this") {
+if (process.argv[2] === "movie-this") {
 
-    var movieName = "";
-
-
-    for (var i = 2; i < process.argv.length; i++) {
-
-        if (i > 2 && i < process.argv.length) {
-          movieName = movieName + "+" + process.argv[i];
-        }
-        else {
-          movieName += process.argv[i];
-      
-        }
-      }
-      
+    
 
 
-axios.get("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy").then(
-  function(response) {
-    console.log(response);
- });
+axios.get("http://www.omdbapi.com/?t=" + term + "&y=&plot=short&apikey=trilogy").then(
+function(response) {
+ 
+  console.log("\nTitle: " + response.data.Title + "\nRelease year: " + response.data.Year + "\nIMBD Rating:" +response.data.imdbRating
+  + "\nRotten Tomatoes Score: " + response.data.Ratings[2].Value + "\nWhere it was produced: " +response.data.Country +
+  "\nLanguage: "  + response.data.Language + "\nPlot: " +response.data.Plot + "\nActors: " + response.data.Actors); 
+}).catch(function(err) {
+  console.log(err);
+});
 
- }
- if (process.argv[2] === "do-what-it-says") {};
+}
+if (process.argv[2] === "do-what-it-says") {};
+
+
 
 
